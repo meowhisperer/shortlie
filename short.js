fdb4ad04415af7922845cb2c0ab558d1f0acb56a
meowhisperer <amauryakpop@gmail.com>
2020-08-14 11:25:51 +0530

    making echo

--- short.js
+++ short.js
@@ -1,4 +1,11 @@
 const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
+const keyboard = Markup.inlineKeyboard([
+  Markup.urlButton('❤️', 'http://telegraf.js.org'),
+  Markup.callbackButton('Delete', 'delete')
+])
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
@@ -9,5 +16,9 @@ bot.command([`start`, `help`], (ctx) => {
         `Hi, I'm a test bot and I'm working`
     );
 });
+
+bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message, Extra.markup(keyboard)))
+bot.action('delete', ({ deleteMessage }) => deleteMessage())
+
 bot.launch()
 console.log(`Bot started...`);
